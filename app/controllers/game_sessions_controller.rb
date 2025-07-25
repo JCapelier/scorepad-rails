d22163aa3c5263879921369e4e158df8e518fce8
Jonas Capelier <capelier.jonas@gmail.com>
2025-07-25 12:03:14 +0200

    Create a basic game session

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,7 +1,20 @@
 class GameSessionsController < ApplicationController
-  def new
+  def create
+    game = Game.find(params[:game_session][:game_id])
+    session = GameSession.new(session_params.merge(starts_at: Time.current, status: "pending"))
+    session.game = game
+
+
+    if session.save
+      redirect_to seating_game_session_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def update
+  private
+
+  def session_params
+    params.require(:game_session).permit(:number_of_players, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :users, only: [:show]
-  resources :games, only: [:show] do
-    resources :game_sessions, only: [:new]
+
+  resources :game_sessions, only: [:create] do
+    member do
+      get :seating, as: :seating
+      patch :assign_seats, as: :assign_seats
+    end
   end
+
+  resources :games, only: [:show]
   resources :game_sessions, only: [:update]
 end
