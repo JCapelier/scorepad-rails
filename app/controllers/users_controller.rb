02d577473cc18d71f8ff7c135ce01fa93404e6cb
Jonas Capelier <capelier.jonas@gmail.com>
2025-08-21 16:44:55 +0200

    fixed dock, added a settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = current_user
   end
@@ -11,5 +13,42 @@ class UsersController < ApplicationController
       usernames = User.where("unaccent(username) ILIKE unaccent(?)", "%#{input}%").limit(10)
       render json: usernames.map { |user| { id: user.id, username: user.username, avatar_url: user.avatar_url } }
     end
+  end
+
+  def update
+    @user = current_user
+
+    # Handle password update
+    if params[:user][:current_password].present? || params[:user][:password].present?
+      if @user.update_with_password(password_params)
+        bypass_sign_in(@user) # Devise: keep user signed in after password change
+        flash[:notice] = "Password updated successfully."
+        redirect_to settings_user_path(@user)
+        return
+      else
+        flash.now[:alert] = "Could not update password."
+        render :settings, status: :unprocessable_entity
+        return
+      end
+    end
+
+    # Handle avatar and username update
+    if @user.update(user_params)
+      flash[:notice] = "Account updated successfully."
+      redirect_to settings_user_path(@user)
+    else
+      flash.now[:alert] = "Could not update account."
+      render :settings, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :avatar)
+  end
+
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'users/autocomplete', to: 'users#autocomplete'
   root to: "pages#home"
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update] do
+    get :settings, on: :member
+  end
   resources :games, only: [:show]
   resources :game_sessions, only: [:new, :create, :update, :destroy]
   resources :scoresheets, only: [:show, :update] do
