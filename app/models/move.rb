4fe6661027b87a12e90ea8832fa58d5eeaf657f5
Jonas Capelier <capelier.jonas@gmail.com>
2025-09-15 17:28:33 +0200

    Added back-end validations

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,5 @@
 class Move < ApplicationRecord
+  validates :move_type, inclusion: { in: %w[first_finisher bid tricks], message: "Move type must be first_finisher, bid, or tricks" }
   belongs_to :session_player
   belongs_to :round
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,5 @@
 class Round < ApplicationRecord
+  validates :status, inclusion: { in: %w[pending active completed], message: "Status must be pending, active, or completed" }
   belongs_to :scoresheet
   delegate :game_session, to: :scoresheet
   delegate :session_players, to: :scoresheet
