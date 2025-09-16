d93baffa9e0168702d5027b812a1e095651390ae
Jonas Capelier <capelier.jonas@gmail.com>
2025-09-16 23:35:49 +0200

    index.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
-pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "turbo_ujs_polyfill", to: "turbo_ujs_polyfill.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "application"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/+esm"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.3-1/lib/assets/compiled/rails-ujs.js"
-pin "turbo_ujs_polyfill", to: "turbo_ujs_polyfill.js"
