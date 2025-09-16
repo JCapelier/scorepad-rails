84945cf1139786856c93b99deb273685a56608b7
Jonas Capelier <capelier.jonas@gmail.com>
2025-09-16 17:11:06 +0200

    Added polyfill to importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -7,3 +6,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/+esm"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.3-1/lib/assets/compiled/rails-ujs.js"
+pin "turbo_ujs_polyfill"
