a97c092916c3dc909a401b448b92cd22acdac5d5
Jonas Capelier <capelier.jonas@gmail.com>
2025-09-21 12:01:33 +0200

    Added a dependent destroy

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,7 +3,7 @@ class Round < ApplicationRecord
   belongs_to :scoresheet
   delegate :game_session, to: :scoresheet
   delegate :session_players, to: :scoresheet
-  has_many :moves
+  has_many :moves, dependent: :destroy
 
   def move_for_first_finisher
     moves.find_by(move_type: "first_finisher")
